f59e02c099fe5c838b96ed6a03ae32fe90dd110f
Evan Cameron <evancameron@cslab02.wheaton.edu>
2018-03-13 16:55:44 -0500

    prim works

--- alg/PrimMinSpanTree.java
+++ alg/PrimMinSpanTree.java
@@ -36,7 +36,54 @@ public class PrimMinSpanTree implements MinSpanTree {
         for (int i = 0; i < g.numVertices(); i++)
             parents[i] = -1;
         
-         //Add code here in part 5
+        records[0].setDistance(0);
+        pq.increaseKey(records[0]);
+        parents[0] = 0;
+        
+        // iterate through the vertices
+        while(!pq.isEmpty()) {
+        	VertexRecord vr = pq.extractMax();
+
+        	int vertex = vr.id;
+
+
+        	
+        	for(Integer v: g.adjacents(vertex)) {
+        		System.out.println("current vertex: " + vertex);
+        		System.out.println("vertex: " + v + " has old parent: " + parents[v]);
+
+
+        		if(v != vertex) {
+        		double distOld = records[v].getDistance();
+        		System.out.println("distOld for vertex (" + v + "):" + distOld);
+
+        		double distNew = g.weight(vertex, v);
+        		System.out.println("distNew for vertex (" + v + "):" + distNew);
+
+        		
+        		
+        		
+        		
+        		if(distOld > distNew) {
+        			records[v].setDistance(distNew);
+        	        pq.increaseKey(records[v]);
+        			parents[v] = vertex;
+        		}
+        		System.out.println("vertex: " + v + " has new parent: " + parents[v]);
+        	}
+        		
+        	}
+        	
+
+        	WeightedEdge edge = new WeightedGraph.WeightedEdge(parents[vertex], vertex, records[vertex].getDistance(), true);
+
+        	System.out.println("weight added: " + edge.weight);
+        	System.out.println("__________________");
+
+        	mstEdges.add(edge);
+        	
+        	
+        }
         return mstEdges;
     }
 
